27f6e87637dd4a9007252575b4770d7062bb4938
Adbhut <adbhutmani301@gmail.com>
2024-07-25 00:14:14 +0530

    update

---
+++ run/src/hom1.java
@@ -0,0 +1,15 @@
+public class hom1
+{
+    public static void main(String[] args)
+    {
+        int age = 22;
+        if (age>=18){
+            System.out.println("you can drive and vote");
+        }
+        else{
+            System.out.println("you can not drive and vote");
+        }
+
+    }
+}
+
