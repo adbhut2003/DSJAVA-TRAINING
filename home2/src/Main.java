dc8386fd566d1ccaf13d937eee570f44f7c2db3b
Adbhut <adbhutmani301@gmail.com>
2024-07-25 00:20:42 +0530

    update

---
+++ home2/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 22;
+        if (age>=18)
+        {
+            System.out.println("you can drive and vote");
+        }
+        else{
+            System.out.println("you can not drive and vote");
+        }
+
+    }
+}
+
+
