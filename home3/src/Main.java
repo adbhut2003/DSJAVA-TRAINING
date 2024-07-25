018ac55e0ad2cc82b2078f05b885f8d54905bbae
Adbhut <adbhutmani301@gmail.com>
2024-07-25 09:32:26 +0530

    update

---
+++ home3/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int age = 16;
+        if (age >= 18){
+            System.out.println("you are adult");
+        }
+        if (age > 13 && age < 18){
+            System.out.println(" you are teenager");
+        }
+        else {
+            System.out.println("you are not adult");
+        }
+    }
+}
+
