4eba05b81d577f5bf4461b839876771b051c53cd
Adbhut <adbhutmani301@gmail.com>
2024-07-25 09:34:57 +0530

    update

---
+++ home 4/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 3;
+        if (A>=B){
+            System.out.println("A is largest of two");
+        } else {
+            System.out.println("B is largest of two ");
+        }
+
+    }
+}
+
